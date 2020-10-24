119c3b6b105e25a03e3e0098db66d96684015329
Soufiane CHAOUFI <soufiane@apploi.com>
2020-10-24 18:18:49 +0100

    foodapp

--- FoodTracker/app.py
+++ FoodTracker/app.py
@@ -1,19 +1,8 @@
 from flask import Flask, render_template, g, request
-import sqlite3
+from datetime import datetime
+from database import connect_db, get_db
 
 app = Flask(__name__)
-
-
-def connect_db():
-    sql = sqlite3.connect('./food_log.db')
-    sql.row_factory = sqlite3.Row
-    return sql
-
-
-def get_db():
-    if not hasattr(g, 'sqlite3_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
 
 
 @app.teardown_appcontext
@@ -22,30 +11,83 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    db = get_db()
+    if request.method == 'POST':
+        date = request.form['date']
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        database_date = datetime.strftime(dt, '%Y%m%d')
+        db.execute('insert into log_date (entry_date) values (?)',
+                   [database_date])
+        db.commit()
+    cursor = db.execute(
+        'select log_date.entry_date , sum(food.protein) as protein, sum(food.carbohydrates) as carbohydrates, sum(food.fat) as fat, sum(food.calories) as calories from log_date left join food_date on food_date.log_date_id = log_date.id left join food on food.id = food_date.food_id order by log_date.entry_date')
+    results = cursor.fetchall()
+    date_results = []
+    for i in results:
+        single_date = {}
+        single_date['entry_date'] = i['entry_date']
+        single_date['protein'] = i['protein']
+        single_date['carbohydrates'] = i['carbohydrates']
+        single_date['fat'] = i['fat']
+        single_date['calories'] = i['calories']
+        d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
+        single_date['entry_date'] = datetime.strftime(d, '%B %d, %Y')
+        single_date['date'] = i['entry_date']
+        date_results.append(single_date)
+    return render_template('home.html', results=date_results)
 
 
-@app.route('/view')
-def view():
-    return render_template('day.html')
+@app.route('/view/<date>', methods=['POST', 'GET'])  # date should be YYYYMMDD
+def view(date):
+    db = get_db()
+    log_date_cursor = db.execute(
+        'select id, entry_date from log_date where entry_date = ?', [date])
+    date_result = log_date_cursor.fetchone()
+    if request.method == 'POST':
+        food = request.form['food_select']
+        food_id = date_result['id']
+        db.execute('insert into food_date (food_id, log_date_id) values (?,?)', [
+            food, food_id])
+        db.commit()
+    d = datetime.strptime(str(date_result['entry_date']), '%Y%m%d')
+    pretty_date = datetime.strftime(d, '%B %d, %Y')
+    food_cursor = db.execute('select id, name from food')
+    food_results = food_cursor.fetchall()
+
+    log_cur = db.execute(
+        'select food.name, food.protein, food.carbohydrates, food.fat, food.calories from log_date left join food_date on food_date.log_date_id = log_date.id left join food on food.id = food_date.food_id where log_date.entry_date = ?', [date])
+    log_results = log_cur.fetchall()
+    totals = {
+        'protein': 0,
+        'carbohydrates': 0,
+        'fat': 0,
+        'calories': 0
+    }
+    for food in log_results:
+        totals['protein'] += food['protein']
+        totals['carbohydrates'] += food['carbohydrates']
+        totals['fat'] += food['fat']
+        totals['calories'] += food['calories']
+    return render_template('day.html', entry_date=date_result['entry_date'], pretty_date=pretty_date, foods=food_results, log_results=log_results, totals=totals)
 
 
 @app.route('/food', methods=['GET', 'POST'])
 def food():
+    db = get_db()
     if request.method == "POST":
         name = request.form['food-name']
         protein = int(request.form['protein'])
         carbohydrates = int(request.form['carbohydrates'])
         fat = int(request.form['fat'])
         calories = protein * 4 + carbohydrates * 4 + fat * 9
-
-        db = get_db()
         db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?,?,?,?,?)', [
                    name, protein, carbohydrates, fat, calories])
         db.commit()
-    return render_template('add_food.html')
+    cursor = db.execute('select * from food')
+    results = cursor.fetchall()
+    return render_template('add_food.html', results=results)
 
 
 if __name__ == "__main__":
