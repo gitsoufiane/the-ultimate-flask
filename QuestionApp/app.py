0ab43e3d36823576806c001f98cac7e54a44b34e
Soufiane CHAOUFI <soufiane@apploi.com>
2020-12-13 00:37:27 +0100

    questions answer app

--- QuestionApp/app.py
+++ QuestionApp/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, g, request
+from flask import Flask, render_template, url_for, g, request, session, redirect
 from database import get_db
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 
 @app.teardown_appcontext
@@ -11,14 +13,31 @@ def close_db(error):
         g.sqlite_db.close()
 
 
+def get_current_user():
+    user = None
+    user_resutlt = None
+    if 'user' in session:
+        user = session['user']
+        db = get_db()
+        user_cur = db.execute(
+            'select id,name,password,expert,admin from users where name = ?', [user])
+        user_resutlt = user_cur.fetchone()
+    return user_resutlt
+
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    # user = None
+    # if 'user' in session:
+    #     user = session['user']
+    user = get_current_user()
+    return render_template('home.html', user=user)
 
 
 @app.route('/register', methods=['post', 'get'])
 def register():
     db = get_db()
+    user = get_current_user()
     if request.method == "POST":
         name = request.form['name']
         hashed_password = generate_password_hash(
@@ -26,45 +45,74 @@ def register():
         db.execute('insert into users (name,password,expert,admin) values (?,?,?,?)', [
                    name, hashed_password, 0, 0])
         db.commit()
-        return 'USER CREATED'
-    return render_template('register.html')
+        session['user'] = name
+        return redirect(url_for('index'))
+    return render_template('register.html', user=user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     db = get_db()
+    user = get_current_user()
     if request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
         user_cur = db.execute(
             'select id,name,password from users where name=?', [name])
         user_result = user_cur.fetchone()
-    return render_template('login.html')
+        if check_password_hash(user_result['password'], password):
+            session['user'] = user_result['name']
+            return redirect(url_for('index'))
+        return '<h1>wrong passowrd</h1>'
+    return render_template('login.html', user=user)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 
 @app.route('/question')
 def question():
-    return render_template('question.html')
+    user = get_current_user()
+    return render_template('question.html', user=user)
 
 
-@app.route('/answer')
+@ app.route('/answer')
 def answer():
-    return render_template('answer.html')
+    user = get_current_user()
+    return render_template('answer.html', user=user)
 
 
-@app.route('/ask')
+@ app.route('/ask')
 def ask():
-    return render_template('ask.html')
+    user = get_current_user()
+    return render_template('ask.html', user=user)
 
 
-@app.route('/unanswered')
+@ app.route('/unanswered')
 def unanswered():
-    return render_template('unanswered.html')
+    user = get_current_user()
+    return render_template('unanswered.html', user=user)
 
 
-@app.route('/users')
+@ app.route('/users')
 def users():
-    return render_template('users.html')
+    user = get_current_user()
+    db = get_db()
+    users_cur = db.execute('select id,name,expert,admin from users')
+    users_results = users_cur.fetchall()
+    return render_template('users.html', user=user, users=users_results)
+
+
+@app.route('/promote/<user_id>')
+def promote(user_id):
+    db = get_db()
+    db.execute('update users set expert=1 where id=?', [user_id])
+    db.commit()
+
+    return redirect(url_for('users'))
 
 
 if __name__ == "__main__":
