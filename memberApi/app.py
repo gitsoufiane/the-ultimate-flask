35a3264bb50e581870ef5778d8ba5423f1daea3b
Soufiane CHAOUFI <soufiane@apploi.com>
2020-12-26 21:08:37 +0100

    member api authentication

--- memberApi/app.py
+++ memberApi/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, g,  request, jsonify
 from database import get_db
-
+from functools import wraps
+api_username = 'admin'
+api_password = 'admin'
 
 app = Flask(__name__)
+
+
+def protected(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth and auth.username == api_username and auth.password == api_password:
+            return f(*args, **kwargs)
+        return jsonify({'message': 'authentication failed !'}), 403
+    return decorated
 
 
 @app.teardown_appcontext
@@ -12,6 +24,7 @@ def close_db(error):
 
 
 @app.route('/members', methods=['GET'])
+@protected
 def get_members():
     db = get_db()
     members_cur = db.execute('select id,name,email,level from members')
@@ -21,10 +34,12 @@ def get_members():
         member_dict = {'id': member['id'], 'name': member['name'],
                        'email': member['email'], 'level': member['level']}
         members.append(member_dict)
+
     return jsonify({'members': members})
 
 
 @app.route('/member/<int:member_id>', methods=['GET'])
+@protected
 def get_member(member_id):
     db = get_db()
     member_curs = db.execute(
@@ -35,8 +50,8 @@ def get_member(member_id):
     return jsonify({'member': response})
 
 
-
 @app.route('/member', methods=['POST'])
+@protected
 def add_member():
     new_member_data = request.get_json()
     name = new_member_data['name']
@@ -57,13 +72,31 @@ def add_member():
 
 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
+@protected
 def edit_member(member_id):
-    pass
+    new_member_data = request.get_json()
+    name = new_member_data['name']
+    email = new_member_data['email']
+    level = new_member_data['level']
+    db = get_db()
+    db.execute('update members set name=?, email=?,level=? WHERE id=?', [
+               name, email, level, member_id])
+    db.commit()
+    member_cur = db.execute(
+        'select id,name,email,level from members where id = ?', [member_id])
+    member_result = member_cur.fetchone()
+    response = {'id': member_result['id'], 'name': member_result['name'],
+                'email': member_result['email'], 'level': member_result['level']}
+    return jsonify({'memeber': response})
 
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
+@protected
 def delete_member(member_id):
-    pass
+    db = get_db()
+    db.execute('delete from members where id=?', [member_id])
+    db.commit()
+    return jsonify({'message': 'the member has been deleted'})
 
 
 if __name__ == '__main__':
